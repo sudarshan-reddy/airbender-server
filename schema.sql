39e695317f7a105ed5efc6e3cf1b879c63120740
Raja Bose C Leo M <raja.bose@realimage.com>
2017-05-26 18:18:50 +0530

    Added database schema

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE airbender (
+      id bigint NOT NULL,
+      name character varying NOT NULL,
+      air_quality int NOT NULL,
+      degrees_celsius integer NOT NULL,
+      degrees_farenheit integer NOT NULL,
+      humidity integer NOT NULL,
+      "timestamp" timestamp with time zone NOT NULL
+);
